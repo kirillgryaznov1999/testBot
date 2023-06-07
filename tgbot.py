22f910f60ec3c231e0ef17cc92fba2c760e3e0e8
kirill <kirillgryaznov_11@mail.ru>
2023-06-07 13:25:13 +0300

    Доставка

--- tgbot.py
+++ tgbot.py
@@ -7,12 +7,9 @@ bot = telebot.TeleBot(config.token)
 
 
 
-#@bot.message_handler(content_types=['text'])
+#@bot.message_handler(content_types=['text']) # Эхо бот
 #def echo(message):
 #    bot.send_message(message.chat.id, message.text)
-
-
-
 
 
 @bot.message_handler(commands = ['start'])
@@ -22,13 +19,20 @@ def url(message):
     markup.add(btn)
     bot.send_message(message.from_user.id, "По этой кнопке ты перейдешь в vk", reply_markup = markup)
 
+@bot.message_handler(content_types=['text']) # Ссылка на заказ еды
+def food(message):
+    if message.text == "Хочу есть" or 'Голоден' or "Давай закажем еды":
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text="Яндекс.Еда", url='https://eda.yandex.ru')
+        btn2 = types.InlineKeyboardButton(text="DeliveryClub", url='https://www.delivery-club.ru/')
+        markup.add(btn1, btn2)
+        bot.send_message(message.from_user.id, "Где хочешь заказать?", reply_markup = markup)
 
 @bot.message_handler(content_types=['text'])
 def mes(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     #markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
     btn1 = types.KeyboardButton('Хорошо')
-    
     btn2 = types.KeyboardButton('Не очень')
     markup.add(btn1, btn2)
     bot.send_message(message.from_user.id, 'Лень придумывать', reply_markup=markup)
@@ -36,6 +40,7 @@ def mes(message):
     if message.text == 'Хорошо':
        bot.send_message(message.from_user.id, 'Это хорошо,  ' + '[ссылке](https://habr.com/ru/docs/help/rules/)', parse_mode='Markdown')
         
+
 
 
 
